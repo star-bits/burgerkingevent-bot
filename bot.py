3cfa3c41c447760b37dacd5da76277941b9085f2
star-bits <star-bits@outlook.com>
2023-05-28 17:33:30 +0900

    n chars at a time

--- bot.py
+++ bot.py
@@ -11,27 +11,23 @@ device.shell('input touchscreen tap 200 2050')
 # Method 1: Entire text
 # device.shell(f"input text '{text}'")
 
-# Method 2: 2 chars at a time
-# for i in range(0, len(text), 2):
-#     chars = text[i:i+2]
-#     device.shell(f"input text '{chars}'")
+# Method 2: n chars at a time
+n = 10
+for i in range(0, len(text), n):
+    chars = text[i:i+n]
+    device.shell(f"input text '{chars}'")
 
 # Method 3: 1.5 chars at a time
-toggle = True
-i = 0
-while i < len(text):
-    if toggle:
-        chars = text[i:i+2]
-        i += 2
-    else:
-        chars = text[i]
-        i += 1
-    device.shell(f"input text '{chars}'")
-    toggle = not toggle
-
-# Method 4: 1 char at a time
-# for i in range(0, len(text)):
-#     char = text[i]
-#     device.shell(f"input text '{char}'")
+# toggle = True
+# i = 0
+# while i < len(text):
+#     if toggle:
+#         chars = text[i:i+2]
+#         i += 2
+#     else:
+#         chars = text[i]
+#         i += 1
+#     device.shell(f"input text '{chars}'")
+#     toggle = not toggle
 
 device.shell('input touchscreen tap 1000 1400')
